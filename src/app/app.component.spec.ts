2780f41c22aa8203b4d6d2b6d588aa10dc558056
Ian Jackson <iansjackson8@gmail.com>
2023-02-27 12:11:00 -0500

    fix directory issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ianjackson'`, () => {
+  it(`should have as title 'ianjackson8.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ianjackson');
+    expect(app.title).toEqual('ianjackson8.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ianjackson app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ianjackson8.github.io app is running!');
   });
 });
